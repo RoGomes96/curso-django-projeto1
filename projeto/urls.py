8b36f4d79b99e8a6c092fbed22f02416071de094
Rodrigo Gomes <rogomes96@outlook.com>
2025-04-06 13:59:08 -0300

    Initial tests

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from recipes.views import home, sobre, contato
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),  # Home
+    path('sobre/', sobre),  # /sobre/
+    path('contato/', contato),  # /contato/
+
 ]
---
+++ recipes/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def home(request):
+    return HttpResponse("Home Test")
+
+
+def sobre(request):
+    return HttpResponse("sobre Test")
+
+
+def contato(request):
+    return HttpResponse("contato Test")
