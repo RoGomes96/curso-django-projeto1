afc4fff2e6556d4cd20a64dbce5dc5cd22904f05
Rodrigo Gomes <rogomes96@outlook.com>
2025-04-12 11:16:15 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 # Create your views here.
 
 
 def home(request):
-    return render(request, "recipes/home.html", status=201, context={
-        'Name': 'Rodrigo Gomes'
-    })
-
-
-def sobre(request):
-    return render(request, 'temp.html')
-
-
-def contato(request):
-    return HttpResponse("contato Test")
+    return render(request, "recipes/home.html", status=201)
